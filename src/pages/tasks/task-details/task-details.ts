8afc5933b47fdbb206c86fad45535cc25b653d72
Manar Sarah <manar.sarah@outlook.com>
2018-12-07 21:19:25 +0200

    finish task with ionic storage

--- src/pages/tasks/task-details/task-details.ts
+++ src/pages/tasks/task-details/task-details.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { task } from '../../../app/models/task';
+import { Storage } from '@ionic/storage';
+import { category } from '../../../app/models/category';
 
 /**
  * Generated class for the TaskDetailsPage page.
@@ -15,11 +18,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TaskDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  task : task;
+  categories : category[];
+
+  catName : string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private storage:Storage) {
+    this.task = navParams.get('tas')
+
+    this.storage.get("categories").then((cats)=>{
+      this.categories = cats;
+      this.catName = this.getCat();
+
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TaskDetailsPage');
   }
+  getCat() {
 
+      for(let i=0;i<this.categories.length;i++) {
+        if(this.task.catID == this.categories[i].ID) {
+          return this.categories[i].title;
+        }
+
+      }
+
+  }
 }
